018b83161d4715528e21cc16e6f6ba0a584436f8
kaush <kaushikkarmakar1590@outlook.com>
2025-12-20 18:35:53 +0530

    Fixed minor bugs.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 mainpage = st.Page("mainpage.py", title = "Predict Chruning Customers", icon = "🔍")
-history_view = st.Page("history.py", title = "Previous History", icon = "🕰️")
+history_view = st.Page("history.py", title = "Prediction History", icon = "🕰️")
 data_view = st.Page("dataset.py", title = "Dataset Information", icon = "📋")
 model_view = st.Page("model_info.py", title = "Model Information", icon = "💻")
 pg = st.navigation([mainpage, history_view, data_view, model_view])
